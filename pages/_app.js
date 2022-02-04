b7e2d968d75ea2c8452477ff1f34ae9a52f72568
Christian Frederici <christian.frederici@gmail.com>
2022-02-04 14:58:24 -0300

    fix chat

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  <script src="//code-sa1.jivosite.com/widget/xRbm4yXyhr" async></script>;
   return <Component {...pageProps} />;
 }
 
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,9 +2,15 @@ import Link from 'next/link';
 
 import styles from './styles.module.css';
 
+import Head from 'next/head';
+
 const Menu = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <script src="//code-sa1.jivosite.com/widget/xRbm4yXyhr" async></script>
+      </Head>
+
       <Link href="/Estabelecimentos">Estabelecimentos</Link>
       <Link href="/Cardapio">Cardápio</Link>
     </div>
